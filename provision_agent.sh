c714c3286bfbb66ea849cac3ecd1d7bafd01dd2a
Chen Wang <chenw@andrew.cmu.edu>
2015-12-18 03:28:33 +0000

    Backup agens-control Dec. 17

--- provision_agent.sh
+++ provision_agent.sh
@@ -2,7 +2,7 @@
 srvName=$1
 srvZone=$2
 
-gcloud compute instances create $srvName --image django-cache-agent --machine-type f1-micro --zone $srvZone --scope compute-rw storage-full userinfo-email
+gcloud compute instances create $srvName --image django-coop-cache-agent --machine-type f1-micro --zone $srvZone --scope compute-rw storage-full userinfo-email
 srvIP=$(gcloud compute instances list |grep $srvName |awk '{print $5}')
 echo "ssh into cache agent $srvName with ip $srvIP and do git pull in agens folder and run server on port :8615"
 ssh -i ~/.ssh/google_compute_engine -n Chen@$srvIP "cd agens; git pull; python3 manage.py migrate; screen -dmS cacheagent python3 manage.py runserver 0.0.0.0:8615"
